638da7d578d8992e8b058ba04a71e917893c4de9
Aang <indra.aang.f@gmail.com>
2020-09-25 11:11:01 +0700

    editFileIndex.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const start = async () => {
 					useChrome: true, // If false will use Chromium instance
 					debug: false, // Opens a debug session
 					logQR: true, // Logs QR automatically in terminal
-					browserArgs: [ '' ], // Parameters to be added into the chrome browser instance
+					browserArgs: [ '--no-sandbox' ], // Parameters to be added into the chrome browser instance
 					refreshQR: 15000, // Will refresh QR every 15 seconds, 0 will load QR once. Default is 30 seconds
 					autoClose: false, // Will auto close automatically if not synced, 'false' won't auto close. Default is 60 seconds (#Important!!! Will automatically set 'refreshQR' to 1000#)
 					disableSpins: true // Will disable Spinnies animation, useful for containers (docker) for a better log
