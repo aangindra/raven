bd677df14601d81fd9e7aaa10d41947e6fe3db94
aangindra <indra.aang.f@gmail.com>
2021-11-30 12:14:22 +0700

    fix index

--- index.js
+++ index.js
@@ -73,19 +73,21 @@ const start = async () => {
       try {
         const venomOptions = {
           multidevice: false, // for version not multidevice use false.(default: true)
-          folderNameToken: "tokens",
-          mkdirFolderToken: "",
-          headless: true,
-          devtools: false,
-          useChrome: true,
-          debug: false,
-          logQR: false,
-          browserArgs: ["--no-sandbox"],
-          refreshQR: 15000,
-          autoClose: false,
-          disableSpins: true,
-          disableWelcome: true,
-          createPathFileToken: false
+          folderNameToken: 'tokens', //folder name when saving tokens
+          mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+          headless: true, // Headless chrome
+          devtools: false, // Open devtools by default
+          useChrome: true, // If false will use Chromium instance
+          debug: false, // Opens a debug session
+          logQR: true, // Logs QR automatically in terminal
+          browserWS: '', // If u want to use browserWSEndpoint
+          browserArgs: ['--no-sandbox'], //Original parameters  ---Parameters to be added into the chrome browser instance
+          puppeteerOptions: {}, // Will be passed to puppeteer.launch
+          disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+          disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+          updatesLog: true, // Logs info updates automatically in terminal
+          autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+          createPathFileToken: false //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
         };
         const client = await new Promise((resolve, reject) => {
           venom.create(
