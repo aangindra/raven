5d1b171b49eed21f147296505c33eb9010052528
aangindra <indra.aang.f@gmail.com>
2021-11-30 13:00:05 +0700

    fix tokens

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const start = async () => {
         const venomOptions = {
           multidevice: false, // for version not multidevice use false.(default: true)
           folderNameToken: 'tokens', //folder name when saving tokens
-          mkdirFolderToken: '/log_qr', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+          mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
           headless: true, // Headless chrome
           devtools: false, // Open devtools by default
           useChrome: true, // If false will use Chromium instance
@@ -87,7 +87,7 @@ const start = async () => {
           disableWelcome: true, // Will disable the welcoming message which appears in the beginning
           updatesLog: true, // Logs info updates automatically in terminal
           autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-          createPathFileToken: false //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+          createFileToken: true //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
         };
         const client = await new Promise((resolve, reject) => {
           venom.create(
